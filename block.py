28026100c1ae995bfa4f987b18c40c6aa23d4751
drewrice2 <drewrice2@gmail.com>
2017-09-04 17:26:25 -0500

    added block.py

---
+++ block.py
@@ -0,0 +1,23 @@
+import hashlib
+import datetime
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash, nonce):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.nonce = nonce
+        self.hash = self.hash_block()
+
+    def hash_block(self):
+        # basic hashing
+        sha = hashlib.sha256()
+        sha.update(
+            str(self.index).encode('utf-8') +
+            str(self.timestamp).encode('utf-8') +
+            str(self.data).encode('utf-8') +
+            str(self.previous_hash).encode('utf-8') +
+            str(self.nonce).encode('utf-8')
+            )
+        return sha.hexdigest()
